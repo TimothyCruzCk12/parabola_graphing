05440efef90ec9c92453cb35f2479b1fb2c49c29
User <user@example.com>
2026-02-24 15:32:18 -0800

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ParabolaGraphing from './components/ParabolaGraphing'
+
+
+function App() {
+  return (
+      <div className="App">
+        <ParabolaGraphing />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/ParabolaGraphing.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const ParabolaGraphing = () => {
+
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default ParabolaGraphing;
